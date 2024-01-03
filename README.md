6a37b8900195f7b38790ae7461993cf6f2facbd9
Kurt Horvath <kurtklaus.horvath@gmail.com>
2024-01-03 22:33:09 +0100

    add discovery process logic

--- README.md
+++ README.md
@@ -6,3 +6,27 @@
 set GOPROXY=https://proxy.golang.org,direct 
 set GOROOT=C:\Program Files\Go
 set GOSUMDB=sum.golang.org
+
+
+# others
+docker run -d --name=dev-consul -p 8500:8500 -v c:\dev\consul\data:/consul/data -v c:\dev\consul\config:/consul/config consul:1.15.4 agent -server -dev -ui -client=0.0.0.0 -bind=0.0.0.0
+
+docker run -d --name=dev-consul1 -p 8500:8500 -p 8600:8600 consul:1.15.4 agent -server -dev -ui -client=0.0.0.0 -bind=0.0.0.0
+
+
+
+# consujl dev docu
+docker run -d -p 8500:8500 -p 8600:8600/udp --name=badger consul:1.15.4 agent -server -ui -node=server-1 -bootstrap-expect=1 -client=0.0.0.0
+
+docker exec badger consul members
+
+docker run --name=fox consul:1.15.4 agent -node=client-1 -retry-join='172.17.0.3'
+
+docker run -p 9001:9001 -d --name=weasel hashicorp/counting-service:0.0.2
+
+docker exec fox /bin/sh -c 'echo '{\'service\': {\'name\': \'counting\', \'tags\': [\'go\'], \'port\': 9001}}' >> /consul/config/counting.json'
+
+docker exec fox consul reload
+
+
+consul watch -type=service -service=counting /home/consul/myservice.sh
--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 	"time"
 
 	"github.com/manifoldco/promptui"
 )
 
 type Location struct {
+	Lat          float64
+	Lon          float64
+	LocationDesc string
+	L1           string
+	L2           string
+	L3           string
+	L4           string
 }
 
 type client struct {
@@ -17,10 +29,98 @@ type client struct {
 	DoneInit       bool
 	updateInterval time.Duration
 	loc            Location
+	baseURL        string
 }
 
-func (c *client) discoveryProcess() {
+func TurnOnKthBit(n, k int) int {
+	return n | (1 << (k))
+}
+
+func (c *client) inWhichZonesIsUserLocated() bool {
+	//send location to API to retrieve zones
+	resp, err := http.Get("http://neverssl.com")
+
+	if err != nil {
+		log.Printf("Request Failed: %s", err)
+		return false
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Reading body failed: %s", err)
+		return false
+	}
+	body = []byte(`["uni.waidmannsdorf.klagenfurt.austria","waidmannsdorf.klagenfurt.austria","klagenfurt.austria", "austria"]`)
+	var arr []string
+	_ = json.Unmarshal(body, &arr)
+	log.Printf("Unmarshaled: %v", arr)
+	fmt.Println(arr)
+
+	for _, element := range arr {
+		switch strings.Count(element, ".") {
+		case 1:
+			fmt.Println("L1")
+			c.loc.L1 = element
+		case 2:
+			fmt.Println("L2")
+			c.loc.L2 = element
+		case 3:
+			fmt.Println("L3")
+			c.loc.L3 = element
+		case 4:
+			fmt.Println("L4")
+			c.loc.L4 = element
+		}
+	}
+
+	if c.loc.L1 != "" {
+		c.loc.LocationDesc = c.loc.L1
+	}
+	if c.loc.L2 != "" {
+		c.loc.LocationDesc = c.loc.L2
+	}
+	if c.loc.L3 != "" {
+		c.loc.LocationDesc = c.loc.L3
+	}
+	if c.loc.L4 != "" {
+		c.loc.LocationDesc = c.loc.L4
+	}
+	fmt.Println("LD", c.loc)
 	return true
+}
+
+func (c *client) areLocationDescriptorsValid() bool {
+	//todo
+	return true
+}
+
+func (c *client) downloadTargetApplication() bool {
+	var URL string
+	URL = c.loc.LocationDesc + "." + c.baseURL
+	fmt.Println("download ..." + URL)
+	return true
+}
+
+func (c *client) discoveryProcess() bool {
+	//lookup geofence based on location
+	var ret bool
+	ret = c.inWhichZonesIsUserLocated()
+	//if ret == false
+	//return ret
+
+	//validate location descriptors
+	ret = c.areLocationDescriptorsValid()
+	//if ret == false
+	//	return ret
+
+	//download target app from edge server
+	ret = c.downloadTargetApplication()
+
+	if ret == false {
+		return false
+	}
+	return true
+
 }
 
 func (c *client) process() {
@@ -40,6 +140,7 @@ func (c *client) process() {
 				return
 			case t := <-ticker.C:
 				fmt.Println("Tick at", t)
+				c.downloadTargetApplication()
 			}
 		}
 	}()
@@ -76,8 +177,8 @@ func listClientHandler() {
 func addClientHandler() {
 	var INDEX int
 	INDEX = len(arrClients)
-
-	arrClients = append(arrClients, client{INDEX, "test", false, false, 3})
+	LOC := Location{0, 0, "", "", "", "", ""}
+	arrClients = append(arrClients, client{INDEX, "test", false, false, 3, LOC, "app.service.consul"})
 	arrClients[len(arrClients)-1].Start()
 }
 
